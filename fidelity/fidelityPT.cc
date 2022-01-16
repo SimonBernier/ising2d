cef0ab46854e53449d0e1a65e3a6fecb55bdf55d
Simon Bernier <simbernier729@gmail.com>
2022-01-15 20:31:09 -0500

    .,

--- fidelity/fidelityPT.cc
+++ fidelity/fidelityPT.cc
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
     {
     std::vector<int> Ly={3, 5, 7, 9};
     std::vector<int> Lx={8, 16, 24, 32, 40, 48, 56, 64};
-    std::vector<double> h = {2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2};
-    std::vector<double> dh = {0.05};
+    std::vector<double> h = {2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2};
+    std::vector<double> dh = {0.025};
     int A = Ly.size(), B = Lx.size(), C = h.size(), D = dh.size();
     int runs = A*B*C*D;
     std::vector<int> Ly_list(runs), Lx_list(runs);
@@ -49,9 +49,12 @@ void runPT(int Ly, int Lx, double h, double dh)
         std::cerr << "Error: file could not be opened" << std::endl;
         exit(1);
     }
-    dataFile << "en0" << " " << "M0" << " " << "var0" << " " << "maxBondDim0" << " "
-             << "en1" << " " << "M1" << " " << "var1" << " " << "maxBondDim1" << " " << "overlap01" << " "
-             << "en2" << " " << "M2" << " " << "var2" << " " << "maxBondDim2" << " " << "overlap02" << " " << std::endl;
+    dataFile << "en0" << " " << "M0" << " " << "p0" << " " << "var0" << " " << "maxBondDim0" << " "
+             << "en1" << " " << "M1" << " " << "p1" << " " << "var1" << " " << "maxBondDim1" << " "
+             << "en2" << " " << "M2" << " " << "p2" << " " << "var2" << " " << "maxBondDim2" << " "
+             << "en3" << " " << "M3" << " " << "p3" << " " << "var3" << " " << "maxBondDim3" << " " << "F03" << " " << "F13" << " "
+             << "en4" << " " << "M4" << " " << "p4" << " " << "var4" << " " << "maxBondDim4" << " " << "F04" << " " << "F14" << " "
+             << "en5" << " " << "M5" << " " << "p5" << " " << "var5" << " " << "maxBondDim5" << " " << std::endl;
 
 
     auto N = Lx * Ly;
@@ -68,6 +71,12 @@ void runPT(int Ly, int Lx, double h, double dh)
         ampo += -2.0*(h-dh), "Sx", j;
     }
 
+    // make parity operator
+    auto P = MPO(N);
+    for(auto j : range1(N)){
+        P.ref(j) = 2.0*sites.op("Sx",j);
+    }
+
     // make vector of Sz operators
     std::vector<ITensor> Sz(N);
     for(auto j : range1(N)){
@@ -77,14 +86,14 @@ void runPT(int Ly, int Lx, double h, double dh)
     // 2d ising model parameters
     auto sweeps = Sweeps(10);
     sweeps.maxdim() = 20, 50, 100, 256, 512;
-    sweeps.cutoff() = 1E-12;
+    sweeps.cutoff() = 1E-10;
 
     //
     //solve for ground state of h-dh
     //
+    // first h-dh state
     auto H = toMPO(ampo);
     auto [en0,psi0] = dmrg(H,randomMPS(sites),sweeps,{"Silent=",true});
-    PrintData(totalQN(psi0));
     auto var = inner(psi0,H,H,psi0)-en0*en0;
     auto maxBondDim = maxLinkDim(psi0);
     double M0 = 0.0;
@@ -94,21 +103,19 @@ void runPT(int Ly, int Lx, double h, double dh)
         M0 += m;
     }
     M0 /= double(N);
-    auto sgnM0 = (M0 > 0) - (M0 < 0);
-    println("h-dh state");
-    printfln("Energy = %0.3f, FM OP = %0.3f, maxLinkDim = %d, var = %0.3g", en0, M0, maxBondDim, var);
+    auto p0 = inner(psi0,P,psi0);
+    auto sgnp0 = (p0 > 0) - (p0 < 0);
+    println("\nfirst h-dh state");
+    printfln("Energy = %0.3f, M = %0.3f, parity = %0.3f, maxLinkDim = %d, var = %0.3g", en0, M0, p0, maxBondDim, var);
 
-    dataFile << en0 << " " << M0 << " " << var << " " << maxBondDim << " ";
+    dataFile << en0 << " " << M0 << " " << p0 << " " << var << " " << maxBondDim << " ";
 
     //
-    // calculate ground state of h+dh
+    // second h-dh state. Is the excited state for PM regime, second GS for FM regime
     //
-    // update Hamiltonian to correct transverse field value by adding 2*dh
-    for(auto j : range1(N)){
-        ampo += -4.0*dh, "Sx", j;
-    }
-    H=toMPO(ampo);
-    auto [en1,psi1] = dmrg(H,randomMPS(sites),sweeps,{"Silent=",true});
+    auto wfs = std::vector<MPS>(1);
+    wfs.at(0) = psi0;
+    auto [en1,psi1] = dmrg(H,wfs,randomMPS(sites),sweeps,{"Silent=",true,"Weight=",20.0});
     var = inner(psi1,H,H,psi1)-en1*en1;
     maxBondDim = maxLinkDim(psi1);
     double M1 = 0.0;
@@ -118,19 +125,17 @@ void runPT(int Ly, int Lx, double h, double dh)
         M1 += m;
     }
     M1 /= double(N);
-    auto sgnM1 = (M1 > 0) - (M1 < 0);
-    auto F = abs(inner(psi1, psi0));
-    println("first h+dh state");
-    printfln("Energy = %0.3f, FM OP = %0.3f, maxLinkDim = %d, var = %0.3g", en1, M1, maxBondDim, var);
-    printfln("fidelity = %0.5g", F);
+    auto p1 = inner(psi1,P,psi1);
+    println("\nsecond h-dh state");
+    printfln("Energy = %0.3f, M = %0.3f, parity = %0.3f, maxLinkDim = %d, var = %0.3g", en1, M1, p1, maxBondDim, var);
 
-    dataFile << en1 << " " << M1 << " " << var << " " << maxBondDim << " " << F << " ";
-    
-    //check if the algorithm finished in the same order parameter sector of the ferromagnetic regime
-    //if not, find the first excited state which will return the second degenerate state
-    if( (abs(M0) > 1E-4) && (sgnM0!=sgnM1) ){
-        auto wfs = std::vector<MPS>(1);
-        wfs.at(0) = psi1;
+    dataFile << en1 << " " << M1 << " " << p1 << " " << var << " " << maxBondDim << " ";
+
+    //
+    // in the ferromagnetic regime, calculate third h-dh state to find the gap
+    //
+    if(p0<0.99){
+        wfs.push_back(psi1);
         auto [en2,psi2] = dmrg(H,wfs,randomMPS(sites),sweeps,{"Silent=",true,"Weight=",20.0});
         var = inner(psi2,H,H,psi2)-en2*en2;
         maxBondDim = maxLinkDim(psi2);
@@ -141,14 +146,103 @@ void runPT(int Ly, int Lx, double h, double dh)
             M2 += m;
         }
         M2 /= double(N);
-        auto sgnM2 = (M2 > 0) - (M2 < 0);
-        F = abs(inner(psi2, psi0));
-        println("second h+dh state");
-        printfln("Energy = %0.3f, FM OP = %0.3f, maxLinkDim = %d, var = %0.3g", en2, M2, maxBondDim, var);
-        printfln("overlap = %0.5g", F);
+        auto p2 = inner(psi2,P,psi2);
+        println("\nthird h-dh state");
+        printfln("Energy = %0.3f, M = %0.3f, parity = %0.3f, maxLinkDim = %d, var = %0.3g", en2, M2, p2, maxBondDim, var);
 
-        dataFile << en2 << " " << M2 << " " << var << " " << maxBondDim << " " << F << " " << std::endl;
+        dataFile << en2 << " " << M2 << " " << p2 << " " << var << " " << maxBondDim << " ";
+        
+    }
+    else{
+        dataFile << "nan" << " " << "nan" << " " << "nan" << " " << "nan" << " " << "nan" << " ";
+    }
 
+    //
+    // calculate ground state of h+dh
+    //
+    // update Hamiltonian to correct transverse field value by adding 2*dh
+    for(auto j : range1(N)){
+        ampo += -4.0*dh, "Sx", j;
+    }
+    H = toMPO(ampo);
+    auto [en3,psi3] = dmrg(H,randomMPS(sites),sweeps,{"Silent=",true});
+    var = inner(psi3,H,H,psi3)-en3*en3;
+    maxBondDim = maxLinkDim(psi3);
+    double M3 = 0.0;
+    for(auto b : range1(N)){
+        psi3.position(b);
+        auto m = elt( dag(prime(psi3(b),"Site")) * Sz[b-1] * psi3(b) );
+        M3 += m;
+    }
+    M3 /= double(N);
+    auto p3 = inner(psi3,P,psi3);
+    auto F03 = abs(inner(psi3,psi0));
+    auto F13 = 0.0;
+    if(p0<0.99){
+        F13 = abs(inner(psi3,psi1));
+    }
+    println("\nfirst h+dh state");
+    printfln("Energy = %0.3f, M = %0.3f, parity = %0.3f, maxLinkDim = %d, var = %0.3g", en3, M3, p3, maxBondDim, var);
+    printfln("Fidelity F03 = %0.5f, Fidelity F13 = %0.5f", F03, F13);
+
+    dataFile << en3 << " " << M3 << " " << p3 << " " << var << " " << maxBondDim << " ";
+    dataFile << F03 << " " << F13 << " ";
+
+    //
+    // second d+dh state
+    //
+    wfs = std::vector<MPS>(1);
+    wfs.at(0) = psi3;
+    auto [en4,psi4] = dmrg(H,wfs,randomMPS(sites),sweeps,{"Silent=",true,"Weight=",20.0});
+    var = inner(psi4,H,H,psi4)-en4*en4;
+    maxBondDim = maxLinkDim(psi4);
+    double M4 = 0.0;
+    for(auto b : range1(N)){
+        psi4.position(b);
+        auto m = elt( dag(prime(psi4(b),"Site")) * Sz[b-1] * psi4(b) );
+        M4 += m;
+    }
+    M4 /= double(N);
+    auto p4 = inner(psi4,P,psi4);
+    auto F04 = 0.0;
+    auto F14 = 0.0;
+    if(p0<0.99){ //h-dh in FM regime
+        F04 = abs(inner(psi4,psi0));
+        if(p3<0.99){ //h+dh in FM regime 
+            F14 = abs(inner(psi4,psi1));
+        }
+    }
+    println("\nsecond h+dh state");
+    printfln("Energy = %0.3f, M = %0.3f, parity = %0.3f, maxLinkDim = %d, var = %0.3g", en4, M4, p4, maxBondDim, var);
+    printfln("Fidelity F04 = %0.5f, Fidelity F14 = %0.5f", F04, F14);
+
+    dataFile << en4 << " " << M4 << " " << p4 << " " << var << " " << maxBondDim << " ";
+    dataFile << F04 << " " << F14 << " ";
+
+    //
+    // in the ferromagnetic regime, calculate third h-dh state to find the gap
+    //
+    if(p3<0.99){
+        wfs.push_back(psi4);
+        auto [en5,psi5] = dmrg(H,wfs,randomMPS(sites),sweeps,{"Silent=",true,"Weight=",20.0});
+        var = inner(psi5,H,H,psi5)-en5*en5;
+        maxBondDim = maxLinkDim(psi5);
+        double M5 = 0.0;
+        for(auto b : range1(N)){
+            psi5.position(b);
+            auto m = elt( dag(prime(psi5(b),"Site")) * Sz[b-1] * psi5(b) );
+            M5 += m;
+        }
+        M5 /= double(N);
+        auto p5 = inner(psi5,P,psi5);
+        println("\nthird h+dh state");
+        printfln("Energy = %0.3f, M = %0.3f, parity = %0.3f, maxLinkDim = %d, var = %0.3g\n", en5, M5, p5, maxBondDim, var);
+
+        dataFile << en5 << " " << M5 << " " << p5 << " " << var << " " << maxBondDim << " ";
+        
+    }
+    else{
+        dataFile << "nan" << " " << "nan" << " " << "nan" << " " << "nan" << " " << "nan" << " ";
     }
 
     dataFile.close();
