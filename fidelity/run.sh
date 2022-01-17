93fae2125ad38d92882837a8665a8b3ae105fd13
Simon Bernier <simbernier729@gmail.com>
2022-01-17 12:32:41 -0500

    commit data

--- fidelity/run.sh
+++ fidelity/run.sh
@@ -4,9 +4,10 @@ set=("$@")
 str="Starting run number"
 runNumber=0
 
-for num in {0..4}
+for num in {0..12}
 do
 let "runNumber = $(( set * 13 + num))"  
+echo " "
 echo "$str $runNumber"
 
 ./fidelityPT $runNumber
