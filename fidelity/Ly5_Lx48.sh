0a38af67a28705305e36ad7168f44263c74b5601
Simon Bernier <simbernier729@gmail.com>
2022-01-18 10:43:49 -0500

    added job files for future reference

---
+++ fidelity/Ly5_Lx48.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --account=def-kagarwal
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=0-1:00
+#SBATCH --array=0-57
+#SBATCH --mail-user=simon.bernier@mail.mcgill.ca
+#SBATCH --mail-type=ALL
+runNumber=$$(($SLURM_ARRAY_TASK_ID+58*11))
+./ising2-PT $runNumber
