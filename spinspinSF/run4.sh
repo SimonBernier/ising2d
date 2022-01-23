7ea801bd751ca0688909b54c32bf4808b09b51da
Simon Bernier <simbernier729@gmail.com>
2022-01-23 13:04:15 -0500

    change the run bash files for easy runs in parallel on my pc. really should learn to use this with MPI

---
+++ spinspinSF/run4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set=("$@")
+str="Starting run number"
+runNumber=0
+h=0
+
+for i in $(seq 3.1 0.1 4.0);
+do
+let "runNumber = $(( runNumber + 1 ))"
+echo " "
+echo "$str $runNumber"
+
+./spinspinSF 3 32 $i 100 0.1 2
+
+done
---
+++ spinspinSF/run5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set=("$@")
+str="Starting run number"
+runNumber=0
+h=0
+
+for i in $(seq 0.2 0.2 2.0);
+do
+let "runNumber = $(( runNumber + 1 ))"
+echo " "
+echo "$str $runNumber"
+
+./spinspinSF 3 64 $i 100 0.1 2
+
+done
