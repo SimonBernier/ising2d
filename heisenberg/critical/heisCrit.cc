7ca9085d389df765da514d41e44dca1391c25ef9
Simon Bernier <simbernier729@gmail.com>
2022-04-20 22:08:14 -0400

    added spin correlations

--- heisenberg/critical/heisCrit.cc
+++ heisenberg/critical/heisCrit.cc
@@ -4,6 +4,8 @@ using namespace itensor;
 
 //calculate Von Neumann entanglement entropy
 Real vonNeumannS(MPS, int);
+//calculate spin-spin correlator
+std::tuple<double, double> spinspin(int,int,MPS,SiteSet);
 
 int main(int argc, char *argv[]){
     std::clock_t tStart = std::clock();
@@ -15,16 +17,25 @@ int main(int argc, char *argv[]){
     printfln("N = %d", N);
 
     // We will write into a file with the time-evolved energy density at all times.
-    char schar[64];
-    int n1 = std::sprintf(schar,"N_%d_HeisenbergCritical.dat",N);
-    std::string s1(schar);
-    std::ofstream enerfile;
+    char schar1[64], schar2[64];
+    int n1 = std::sprintf(schar1,"N_%d_heisCritical_En.dat",N);
+    int n2 = std::sprintf(schar2,"N_%d_heisCritical_SSC.dat",N);
+    std::string s1(schar1), s2(schar2);
+    std::ofstream enerfile, sscfile;
+
     enerfile.open(s1); // opens the file
     if( !enerfile ) { // file couldn't be opened
         std::cerr << "Error: file could not be opened" << std::endl;
         exit(1);
     }
     enerfile << "energy" << " " << "var" << " " << "SvN" << " " << "bondDim" << " " << "localEnergy" << " " << std::endl;
+
+    sscfile.open(s2); // opens the file
+    if( !sscfile ) { // file couldn't be opened
+        std::cerr << "Error: file could not be opened" << std::endl;
+        exit(1);
+    }
+    sscfile << "szsz" << " " << "spsm" << " " << std::endl;
     
     auto sites = SpinHalf(N);
     auto state = InitState(sites);
@@ -48,8 +59,8 @@ int main(int argc, char *argv[]){
     auto H = toMPO(ampo);
     
     //sweeps
-    auto sweeps = Sweeps(10); //number of sweeps is 5
-    sweeps.maxdim() = 10,20,100,100,200,200,512; //gradually increase states kept
+    auto sweeps = Sweeps(10); //number of sweeps is 10
+    sweeps.maxdim() = 10,20,100,100,200; //gradually increase states kept
     sweeps.cutoff() = 1E-10; //desired truncation error
 
     // Create the Local Energy Density Tensors
@@ -60,6 +71,9 @@ int main(int argc, char *argv[]){
         LED[b-1] += 0.5*sites.op("S-",b)*sites.op("S+",b+1);
         LED[b-1] += 1.0*sites.op("Sz",b)*sites.op("Sz",b+1);
     }
+
+    // Create the SzSz and S+S- correlation vector
+    std::vector<double> szszcorr(N), spsmcorr(N);
     
     // Find Initial Ground State
     auto [energy,psi] = dmrg(H,initState,sweeps,{"Silent=",true});
@@ -74,7 +88,7 @@ int main(int argc, char *argv[]){
     }
     enerfile << energy << " " << var << " " << SvN << " ";
     IndexSet bonds = linkInds(psi); //get bond dimensions
-    for (int j=0; j<N-1; j++){
+    for (int j = 0; j < N-1; j++){
         enerfile << dim(bonds[j]) << " ";
     }
     for (int j = 0; j < N-1; j++){
@@ -82,9 +96,24 @@ int main(int argc, char *argv[]){
     }
     enerfile << std::endl;
 
+    //calculate spin-spin correlation
+    for (int b = 1; b <= N; b++){
+        auto [szsz,spsm] = spinspin(N/2+1,b,psi,sites);
+        szszcorr[b-1] = szsz;
+        spsmcorr[b-1] = spsm;
+    }
+    //store variables to spin spin correlation file
+    for (int j = 0; j < N; j++){
+        sscfile << szszcorr[j] << " ";
+    }
+    for (int j = 0; j < N; j++){
+        sscfile << spsmcorr[j] << " ";
+    }
+    sscfile << std::endl;
+
     printfln("N = %d, energy = %0.3f, SvN = %0.3f, maxDim = %d", N, energy, SvN, maxLinkDim(psi));
 
-    std::cout<< std::endl << " END PROGRAM. TIME TAKEN :";
+    std::cout<< std::endl << " END PROGRAM. ";
     
     std::printf("Time taken: %.3fs\n", (double)(std::clock() - tStart)/CLOCKS_PER_SEC);
     
@@ -111,3 +140,48 @@ Real vonNeumannS(MPS psi, int b){
     return SvN;
 
 }//vonNeumannS
+
+//calculate spin-spin correlator
+std::tuple<double, double> spinspin(int center, int b, MPS psi, SiteSet sites){
+    
+    double corrZ, corrPM;
+
+    psi.position(b);
+    if(b>center){ //bring site b next to the center from right
+        for(int n=b-1; n>center; n--){
+            auto g = BondGate(sites,n,n+1);
+            auto AA = psi(n)*psi(n+1)*g.gate(); //contract over bond n
+            AA.replaceTags("Site,1","Site,0");
+            psi.svdBond(g.i1(), AA, Fromright); //svd from the right
+            psi.position(g.i1()); //move orthogonality center to the left 
+        }
+        auto ket = psi(center)*psi(center+1);
+        auto SzSz = sites.op("Sz",center)*sites.op("Sz",center+1);
+        auto SpSm = sites.op("S+",center+1)*sites.op("S-",center);
+        corrZ = eltC( dag(prime(ket,"Site")) * SzSz * ket).real();
+        corrPM = eltC( dag(prime(ket,"Site")) * SpSm * ket).real();
+    }
+    else if(b<center){ //bring site b next to the center from left
+        for(int n=b; n<center-1; n++){
+          auto g = BondGate(sites,n,n+1);
+          auto AA = psi(n)*psi(n+1)*g.gate(); //contract over bond n
+          AA.replaceTags("Site,1","Site,0");
+          psi.svdBond(g.i1(), AA, Fromleft); //svd from the right
+          psi.position(g.i2()); //move orthogonality center to the right 
+        }
+        auto ket = psi(center-1)*psi(center);
+        auto SzSz = sites.op("Sz",center-1)*sites.op("Sz",center);
+        auto SpSm = sites.op("S+",center-1)*sites.op("S-",center);
+        corrZ = eltC( dag(prime(ket,"Site")) * SzSz * ket).real();
+        corrPM = eltC( dag(prime(ket,"Site")) * SpSm * ket).real();
+    }
+    else{
+        corrZ = 0.25;
+        auto ket = psi(center);
+        auto SpSm = 0.5*sites.op("Id",center) + sites.op("Sz",center);
+        corrPM = eltC( dag(prime(ket,"Site")) * SpSm * ket).real();
+    }
+
+    return {corrZ, corrPM};
+
+}//Szsz
