0fbc9e4261aab4978dc1cf03f4dcabea6cf503c0
Simon Bernier <simbernier729@gmail.com>
2022-08-20 18:52:49 -0400

    added bash files to help with testing

---
+++ superluminal/runLy2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set=("$@")
+str="Starting run number"
+runNumber=0
+
+for i in $(seq 0 1 26);
+do
+let "runNumber = $(( runNumber + 1 ))"
+echo " "
+echo "$str $runNumber"
+
+./super_2dtfi input_2dtfi_Ly_2_runN_$i
+
+done
